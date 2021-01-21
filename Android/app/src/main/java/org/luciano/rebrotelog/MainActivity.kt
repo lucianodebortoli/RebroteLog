4023c03a783bf75d3bddc97cebe60f0b75617352
Luciano De Bortoli <luciano.debortoli@hipcamailab.com>
2021-01-21 13:20:33 -0300

    init Android Empty Activity

---
+++ Android/app/src/main/java/org/luciano/rebrotelog/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.luciano.rebrotelog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
